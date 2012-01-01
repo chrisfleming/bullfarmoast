e05425e314ee095a1bb27f5834dd77db122c72bc
Chris Fleming <me@chrisfleming.org>
2012-01-01 21:22:54 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bullfarmoast::Application.config.secret_token = '383bde76357af897ae501c71cca9303eaa84b4977956b8d2cb184ea86f89e0f733535c3829e0a677a81da3f7812984939318945260cdfa2f35199fe3d37c006b'
